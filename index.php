7f24c994ea7e4574302f693b55a722e915fc4574
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 00:49:05 +0530

    created the bootstrap .min.css which is used for offliline purpose

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Quiz</title>
+    <link  rel="stylesheet" href="css/bootstrap.min.css"/> 
 </head>
 <body>
-    
+<h1 class="welcome">Welcome All </h1>
 </body>
 </html>
