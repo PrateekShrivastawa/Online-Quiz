3ea51dfcc8b3b6ae7835a1a8b19fc8b55978b1ac
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 01:50:47 +0530

    add form in adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -7,6 +7,28 @@
     <title>Admin Login Here</title>
 </head>
 <body>
-    
+    <?php
+    require_once("header.php");
+    ?>
+<form action="adminlogin.php" name="f1" method="POST">
+<br><br>
+<h2> Head Login Here:</h2>
+<br><br><br>
+<label for="username">Username</label><br><br>
+<div class="input-user">
+          <input class="input100" type="email" id="username" name="un" placeholder="Enter Head Username" required><br>
+</div>
+<br><br>   
+<label for="password">Password</label><br><br>
+<div class="input-user">
+          <input class="input100" type="password" id="password" name="ps" placeholder="Enter Password" required><br>
+</div>
+<br><br><br>
+<div class="input-btn">
+          <button class="btn100" type="submit" name="login">
+            Sign In
+          </button>
+        </div>
+</form>
 </body>
 </html>
