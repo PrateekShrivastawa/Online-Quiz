bd0a51242c2c025d0efacddb8b7552c90133f12a
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 01:03:05 +0530

    update the bootstrap file so that user can go another page

--- index.php
+++ index.php
@@ -9,5 +9,19 @@
 </head>
 <body>
 <h1 class="welcome">Welcome All </h1>
+<nav class="navbar navbar-inverse navbar-fixed-top"> 
+  <div class="container-fluid">
+    <div class="navbar-header">  
+    <a class="navbar-brand" href="index.php">Online Examination System</a>          
+    </div >
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="<?php echo 'adminlogin.php'; ?>" >HEAD</a></li>   
+        <li><a href="<?php echo 'Studentsignin.php'; ?>" >SIGN IN</a></li>
+        <li><a href="<?php echo 'studentsignup.php'; ?>" >SIGN UP</a></li>
+         <li><a href="<?php echo 'teacherlogin.php'; ?>" >TEACHER</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </body>
 </html>
