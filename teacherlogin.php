034c69603f29bfa9a7ffa4709f79937b5094b3f8
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 15:56:52 +0530

    done teacherlogin php page and create the teacherdash

--- teacherlogin.php
+++ teacherlogin.php
@@ -32,8 +32,6 @@ label{
 }
 .input-user{
     text-align:center;
-   
-    
 }
 .input100{
     text-align:center;
@@ -89,6 +87,26 @@ label{
     </form>
 </body>
 </html>
+<?php 
+include_once 'dbConnection.php';
+if(isset($_POST['login'])){
+$ref=@$_GET['q'];
+$email = $_POST['un'];
+$password = $_POST['ps'];
+$result = mysqli_query($con,"SELECT email FROM admin WHERE email = '$email' and password = '$password' and role = 'Teacher'") or die('Error');
+$count=mysqli_num_rows($result);
+if($count==1){
+session_start();
+if(isset($_SESSION['email'])){
+session_unset();}
+$_SESSION["name"] = 'Teacher';
+$_SESSION["key"] ='letspractice';
+$_SESSION["email"] = $email;
+header("location:teacherdash.php");
+}
+else header("location:$ref?w=Warning : Access denied");
+}
+?>
 <?php
 require_once("footer.php");
 ?>
