236c0f1cf35e8a6564488b00ccb0048860f38ded
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 23:43:50 +0530

    create the delete user into the update.php

--- update.php
+++ update.php
@@ -7,6 +7,21 @@
     <title>Update php</title>
 </head>
 <body>
-    
+<?php
+include_once 'dbConnection.php';
+session_start();
+$email=$_SESSION['email'];
+
+//delete user
+if(isset($_SESSION['key'])){
+if(@$_GET['demail'] && $_SESSION['key']=='letspractice') {
+$demail=@$_GET['demail'];
+$r1 = mysqli_query($con,"DELETE FROM rank WHERE email='$demail' ") or die('Error');
+$r2 = mysqli_query($con,"DELETE FROM history WHERE email='$demail' ") or die('Error');
+$result = mysqli_query($con,"DELETE FROM user WHERE email='$demail' ") or die('Error');
+header("location:Admindash.php?q=1");
+}
+}
+?>
 </body>
 </html>
