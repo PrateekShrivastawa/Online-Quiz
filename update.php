4ccc62c91a87ea0c4e4fd3ce1c8d4da419fa1adc
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-22 00:44:21 +0530

    add remove quiz in update.php section

--- update.php
+++ update.php
@@ -33,8 +33,24 @@ if(isset($_SESSION['key'])){
     header("location:Admindash.php?q=5");
     }
     }
+
+//remove quiz
+if(isset($_SESSION['key'])){
+if(@$_GET['q']== 'rmquiz' && $_SESSION['key']=='letspractice') {
+$eid=@$_GET['eid'];
+$result = mysqli_query($con,"SELECT * FROM questions WHERE eid='$eid'") or die('Error');
+while($row = mysqli_fetch_array($result)) {
+	$qid = $row['qid'];
+$r1 = mysqli_query($con,"DELETE FROM options WHERE qid='$qid'") or die('Error');
+$r2 = mysqli_query($con,"DELETE FROM answer WHERE qid='$qid' ") or die('Error');
+}
+$r3 = mysqli_query($con,"DELETE FROM questions WHERE eid='$eid' ") or die('Error');
+$r4 = mysqli_query($con,"DELETE FROM quiz WHERE eid='$eid' ") or die('Error');
+$r4 = mysqli_query($con,"DELETE FROM history WHERE eid='$eid' ") or die('Error');
+
+header("location:teacherdash.php?q=5");
+}
+}
 ?>
-
-
 </body>
 </html>
