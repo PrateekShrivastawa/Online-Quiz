b056dd7c8ad860c1e8b7d22f7febaa6c2e608d81
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-22 00:29:26 +0530

    done teacherdash home page

--- teacherdash.php
+++ teacherdash.php
@@ -5,8 +5,87 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teacher Dash Board</title>
+    <link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link rel="stylesheet" href="css/main.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
 </head>
 <body>
-    <h2>Teacher Dash Board</h2>
+<body  style="background:#eff0da;">
+<div class="header" style="background-color:black">
+<div class="row">
+
+</div>
+<?php
+ include_once 'dbConnection.php';
+session_start();
+$email=$_SESSION['email'];
+  if(!(isset($_SESSION['email']))){
+header("location:index.php");
+}
+else
+{
+$name = $_SESSION['name'];
+include_once 'dbConnection.php';
+echo '<span class="pull-right top title1" ><span class="log1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <a href="Admindash.php" class="log log1">'.$email.'</a>&nbsp;|&nbsp;<a href="logout.php?q=Admindash.php" class="log"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</button></a></span>';
+}?>
+</div>
+<!-- admin start-->
+<!--navigation menu-->
+<nav class="navbar navbar-default title1">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="teacherdash.php?q=0"><b>Dashboard - Teacher</b></a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="Admindash.php?q=0">Home</a></li>
+        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="Admindash.php?q=1">Scores</a></li>
+		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="Admindash.php?q=2">Ranking</a></li>
+        <li class="dropdown <?php if(@$_GET['q']==4 || @$_GET['q']==5) echo'active'; ?>">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quiz<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="Admindash.php?q=4">Add Quiz</a></li>
+            <li><a href="Admindash.php?q=5">Remove Quiz</a></li>
+          </ul>
+        </li>
+      </ul> 
+          </div>
+  </div>
+</nav>
+
+<!--home start-->
+
+<?php if(@$_GET['q']==0) {
+
+$result = mysqli_query($con,"SELECT * FROM quiz where email='$email' ORDER BY date DESC") or die('Error');
+echo  '<div class="panel"><table class="table table-striped title1">
+<tr><td><b>S.N.</b></td><td><b>Topic</b></td><td><b>Total question</b></td><td><b>Marks</b></td><td><b>positive</b></td><td><b>negative</b></td><td><b>Time limit</b></td><td></td></tr>';
+$c=1;
+while($row = mysqli_fetch_array($result)) {
+	$title = $row['title'];
+	$total = $row['total'];
+	$sahi = $row['sahi'];
+  $wrong = $row['wrong'];
+    $time = $row['time'];
+	$eid = $row['eid'];
+$q12=mysqli_query($con,"SELECT score FROM history WHERE eid='$eid' AND email='$email'" )or die('Error98');
+$rowcount=mysqli_num_rows($q12);	
+if($rowcount == 0){
+	echo '<tr><td>'.$c++.'</td><td>'.$title.'</td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$sahi.'</td><td>'.$wrong.'</td><td>'.$time.'&nbsp;min</td>
+	</tr>';
+}
+else
+{
+echo '<tr style="color:#99cc32"><td>'.$c++.'</td><td>'.$title.'&nbsp;<span title="This quiz is already solve by you" class="glyphicon glyphicon-ok" aria-hidden="true"></span></td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
+	</tr>';
+}
+}
+$c=0;
+echo '</table></div>';
+
+}
+?>
+
 </body>
 </html>
