b1fe4501583e55e64c4859e6c8f3c0da7d4d0eac
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 12:32:31 +0530

    done css for studentssignin.php page done

--- Studentsignin.php
+++ Studentsignin.php
@@ -6,6 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student-SignIn Page</title>
     <link rel="stylesheet" href="css/index.css">
+    <style>
+    body{
+    margin:0px;
+    padding:0px;
+    background-image: url('images/adminback01.jpg');
+    }
+h2{
+    text-align:center;
+    color:white;
+    font-family: inherit;
+    font-weight: 500;
+    line-height: 1.1;
+    
+    margin-left: -125px;
+}
+label{
+    display:block;
+    text-align:center; 
+    line-height: 7%;
+    font-size: .85em;
+     margin-left:-313px;
+     font-size:18px;
+     color:white;
+}
+.input-user{
+    text-align:center;
+   
+    
+}
+.input100{
+    text-align:center;
+    border-radius: 18px;
+    width: 408px;
+    height: 32px;
+}
+.input-btn{
+    text-align:center;
+}
+.btn100{
+    border-radius: 34px;
+    width: 209px;
+    height: 40px;
+}
+.btn100:hover{
+    background-color:brown;
+}
+.check{
+    color:white;
+}
+.check1{
+    color:white;
+    font-size:20px;
+}
+</style>
 </head>
 <body>
 <?php
