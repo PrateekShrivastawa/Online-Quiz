7a89e791b4fb65e2355442a17a514dacc33ff4a2
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-21 19:57:32 +0530

    update the name

--- Admindash.php
+++ Admindash.php
@@ -37,14 +37,14 @@ echo '<span class="pull-right top title1" ><span class="log1"><span class="glyph
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="headdash.php?q=0">Home</a></li>
-        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="headdash.php?q=1">Students</a></li>
-		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="headdash.php?q=2">Ranking</a></li>
+        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="Admindash.php?q=0">Home</a></li>
+        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="Admindash.php?q=1">Students</a></li>
+		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="Admindash.php?q=2">Ranking</a></li>
         <li class="dropdown <?php if(@$_GET['q']==4 || @$_GET['q']==5) echo'active'; ?>">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Teacher<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="headdash.php?q=4">Add Teacher</a></li>
-            <li><a href="headdash.php?q=5">Remove Teacher</a></li>
+            <li><a href="Admindash.php?q=4">Add Teacher</a></li>
+            <li><a href="Admindash.php?q=5">Remove Teacher</a></li>
           </ul>
         </li>
       </ul> 
