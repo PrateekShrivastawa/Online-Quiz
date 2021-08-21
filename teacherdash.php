65ad3042506ad4b6d06a820828aff85f6788e655
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-22 00:36:22 +0530

    add ranking page in teacherdash

--- teacherdash.php
+++ teacherdash.php
@@ -109,5 +109,33 @@ echo'</table></div>';
 }
 
 ?>
+
+<!-- Ranking Start -->
+<?php
+if(@$_GET['q']== 2) 
+{
+$q=mysqli_query($con,"SELECT * FROM rank  ORDER BY score DESC " )or die('Error223');
+echo  '<div class="panel title">
+<table class="table table-striped title1" >
+<tr><td><b>Rank</b></td><td><b>Name</b></td><td><b>Gender</b></td><td><b>College</b></td><td><b>Score</b></td></tr>';
+$c=0;
+while($row=mysqli_fetch_array($q) )
+{
+$e=$row['email'];
+$s=$row['score'];
+$q12=mysqli_query($con,"SELECT * FROM user WHERE email='$e' " )or die('Error231');
+while($row=mysqli_fetch_array($q12) )
+{
+$name=$row['name'];
+$gender=$row['gender'];
+$college=$row['college'];
+}
+$c++;
+echo '<tr><td style="color:#99cc32"><b>'.$c.'</b></td><td>'.$name.'</td><td>'.$gender.'</td><td>'.$college.'</td><td>'.$s.'</td><td>';
+}
+echo '</table></div>';}
+
+?>
+
 </body>
 </html>
