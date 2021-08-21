068358b9b5e296f0b9846a84a83df005dd12b32c
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2021-08-22 00:34:40 +0530

    add teacher score details page in teacherdash

--- teacherdash.php
+++ teacherdash.php
@@ -27,7 +27,7 @@ else
 {
 $name = $_SESSION['name'];
 include_once 'dbConnection.php';
-echo '<span class="pull-right top title1" ><span class="log1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <a href="Admindash.php" class="log log1">'.$email.'</a>&nbsp;|&nbsp;<a href="logout.php?q=Admindash.php" class="log"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</button></a></span>';
+echo '<span class="pull-right top title1" ><span class="log1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <a href="teacherdash.php" class="log log1">'.$email.'</a>&nbsp;|&nbsp;<a href="logout.php?q=teacherdash.php" class="log"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</button></a></span>';
 }?>
 </div>
 <!-- admin start-->
@@ -39,14 +39,14 @@ echo '<span class="pull-right top title1" ><span class="log1"><span class="glyph
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="Admindash.php?q=0">Home</a></li>
-        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="Admindash.php?q=1">Scores</a></li>
-		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="Admindash.php?q=2">Ranking</a></li>
+        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="teacherdash.php?q=0">Home</a></li>
+        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="teacherdash.php?q=1">Scores</a></li>
+		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="teacherdash.php?q=2">Ranking</a></li>
         <li class="dropdown <?php if(@$_GET['q']==4 || @$_GET['q']==5) echo'active'; ?>">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quiz<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="Admindash.php?q=4">Add Quiz</a></li>
-            <li><a href="Admindash.php?q=5">Remove Quiz</a></li>
+            <li><a href="teacherdash.php?q=4">Add Quiz</a></li>
+            <li><a href="teacherdash.php?q=5">Remove Quiz</a></li>
           </ul>
         </li>
       </ul> 
@@ -87,5 +87,27 @@ echo '</table></div>';
 }
 ?>
 
+<!-- score details -->
+<?php
+if(@$_GET['q']== 1) 
+{
+  $q=mysqli_query($con,"SELECT distinct q.title,u.name,u.college,h.score,h.date from user u,history h,quiz q where q.email='$email' and q.eid=h.eid and h.email=u.email order by q.eid DESC")or die('Error197');
+echo  '<div class="panel title">
+<table class="table table-striped title1" >
+<tr style="color:black"><td><b>S.N.</b></td><td><b>Title</b></td><td><b>Name</b></td><td><b>College</b></td><td><b>Score<b></td><td><b>Date</b></td>';
+$c=0;
+while($row=mysqli_fetch_array($q) )
+{
+$title=$row['title'];
+$name=$row['name'];
+$college=$row['college'];
+$score=$row['score'];
+$date=$row['date'];
+echo '<tr><td>'.$c++.'</td><td>'.$title.'</td><td>'.$name.'</td><td>'.$college.'</td><td>'.$score.'</td><td>'.$date.'</td></tr>';
+}
+echo'</table></div>';
+}
+
+?>
 </body>
 </html>
